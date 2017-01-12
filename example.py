1a83883e65deea8f69b0568043ac8ed90c2c63ea
Michael Leonard <maybeillrememberit@gmail.com>
2017-01-11 23:25:34 -0600

    update example.py to be slightly cleaner

--- example.py
+++ example.py
@@ -1,29 +1,26 @@
-import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 from PySide.QtUiTools import *
+import sys
 
 
-def load_ui_widget(filename, parent=None):
+def load_ui_widget(filename, parent):
     loader = QUiLoader()
     uifile = QFile(filename)
     uifile.open(QFile.ReadOnly)
-    ui = loader.load(uifile, parent)
+    loader.load(uifile, parent)
     uifile.close()
-
-    return ui
 
 
 class ExampleWidget(QWidget):
     def __init__(self, filepath):
         super(ExampleWidget, self).__init__()
-        self.ui = load_ui_widget(filepath)
+        load_ui_widget(filepath, parent=self)
+        self.show()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     widget = ExampleWidget("example.ui")
-    widget.ui.show()
 
     sys.exit(app.exec_())
