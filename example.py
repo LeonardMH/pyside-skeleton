967082af8bac2ffa9c09632985025b58fb211f60
Michael Leonard <maybeillrememberit@gmail.com>
2017-01-11 23:37:45 -0600

    further clean-up example.py

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 from PySide.QtCore import QFile
 from PySide.QtGui import QWidget, QApplication
 from PySide.QtUiTools import QUiLoader
-import sys
 
 
 def load_ui_widget(filename, parent):
@@ -12,14 +11,23 @@ def load_ui_widget(filename, parent):
     uifile.close()
 
 
+class ExampleApplication(QApplication):
+    def __init__(self, **kwargs):
+        super(ExampleApplication, self).__init__([], **kwargs)
+
+    def start(self):
+        self.exec_()
+
+
 class ExampleWidget(QWidget):
     def __init__(self, filepath):
         super(ExampleWidget, self).__init__()
-        load_ui_widget(filepath, parent=self)
+        load_ui_widget("example.ui", parent=self)
+        self.setWindowTitle("PySide Example")
         self.show()
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = ExampleApplication()
     widget = ExampleWidget("example.ui")
-    sys.exit(app.exec_())
+    app.start()
